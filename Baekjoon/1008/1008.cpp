372bdbf638d57f75c649758422e324847fc8362b
Kaeru224 <tmdn01cs@gmail.com>
2025-07-10 15:11:58 +0900

    [1008] A/B

--- Baekjoon/1008/1008.cpp
+++ Baekjoon/1008/1008.cpp
@@ -1,15 +1,12 @@
+#include <iomanip>
 #include <iostream>
 
 int main() {
   double a, b;
 
-  std::cout.precision(10);
-
-  std::cout << std::fixed;
-
   std::cin >> a >> b;
 
-  std::cout << a / b << std::endl;
+  std::cout << std::fixed << std::setprecision(10) << a / b << std::endl;
 
   return 0;
 }
