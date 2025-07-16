13c871392cb27d4f6e7f0b97a1e27d551aff7a32
Kaeru224 <tmdn01cs@gmail.com>
2025-07-17 01:56:25 +0900

    [2525] 오븐 시계

---
+++ Baekjoon/2525/2525.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int a, b, c;
+
+  std::cin >> a >> b >> c;
+
+  int totalCurrentMinutes = a * 60 + b;
+  int totalEndMinutes = totalCurrentMinutes + c;
+
+  int endHours = (totalEndMinutes / 60) % 24;
+  int endMinutes = totalEndMinutes % 60;
+
+  std::cout << endHours << " " << endMinutes << std::endl;
+
+  return 0;
+}
