97a3a5ee3df200def29734ab270986d079f339d5
Kaeru224 <tmdn01cs@gmail.com>
2025-07-19 12:36:11 +0900

    [10950] A+B - 3

---
+++ Baekjoon/10950/10950.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int t{};
+
+  std::cin >> t;
+
+  for (int i{1}; i <= t; i++) {
+    int a{}, b{};
+
+    std::cin >> a >> b;
+
+    int sum = a + b;
+
+    std::cout << sum << std::endl;
+  }
+
+  return 0;
+}
