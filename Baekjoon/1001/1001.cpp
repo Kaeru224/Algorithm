0244026c1a9f00ffc66735fed8da79981acda44d
Kaeru224 <tmdn01cs@gmail.com>
2025-07-19 12:41:43 +0900

    [1001] A-B

--- Baekjoon/1001/1001.cpp
+++ Baekjoon/1001/1001.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int main() {
-  int a, b;
+  int a{};
+  int b{};
 
   std::cin >> a >> b;
 
-  std::cout << a - b << std::endl;
+  int difference = a - b;
+
+  std::cout << difference << std::endl;
 
   return 0;
 }
