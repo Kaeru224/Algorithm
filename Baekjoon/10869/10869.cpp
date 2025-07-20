e6f38d8e6b362ab629d1a602e007a1e45fffb252
Kaeru224 <tmdn01cs@gmail.com>
2025-07-21 04:53:17 +0900

    [10869] 사칙연산

--- Baekjoon/10869/10869.cpp
+++ Baekjoon/10869/10869.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
 int main() {
-  int a, b;
+  int a{};
+  int b{};
 
   std::cin >> a >> b;
 
-  std::cout << a + b << std::endl;
-  std::cout << a - b << std::endl;
-  std::cout << a * b << std::endl;
-  std::cout << a / b << std::endl;
-  std::cout << a % b << std::endl;
+  int sum = a + b;
+  int difference = a - b;
+  int product = a * b;
+  int quotient = a / b;
+  int remainder = a % b;
+
+  std::cout << sum << std::endl;
+  std::cout << difference << std::endl;
+  std::cout << product << std::endl;
+  std::cout << quotient << std::endl;
+  std::cout << remainder << std::endl;
 
   return 0;
 }
