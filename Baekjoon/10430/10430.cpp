6ad21d10757d95998d0cf8d8ced9e5a88656ca27
Kaeru224 <tmdn01cs@gmail.com>
2025-07-21 04:52:45 +0900

    [10430] 나머지

--- Baekjoon/10430/10430.cpp
+++ Baekjoon/10430/10430.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
 int main() {
-  int a, b, c;
+  int a{};
+  int b{};
+  int c{};
 
   std::cin >> a >> b >> c;
 
-  std::cout << (a + b) % c << std::endl;
-  std::cout << ((a % c) + (b % c)) % c << std::endl;
-  std::cout << (a * b) % c << std::endl;
-  std::cout << ((a % c) * (b % c)) % c << std::endl;
+  int sumModC = (a + b) % c;
+
+  int sumOfMods = ((a % c) + (b % c)) % c;
+
+  int productModC = (a * b) % c;
+
+  int productOfMods = ((a % c) * (b % c)) % c;
+
+  std::cout << sumModC << std::endl;
+  std::cout << sumOfMods << std::endl;
+  std::cout << productModC << std::endl;
+  std::cout << productOfMods << std::endl;
 
   return 0;
 }
