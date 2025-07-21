c0a8e5b07183228ce811e47fa46bca78369c112d
Kaeru224 <tmdn01cs@gmail.com>
2025-07-22 08:03:51 +0900

    [2588] 곱셈

--- Baekjoon/2588/2588.cpp
+++ Baekjoon/2588/2588.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
 int main() {
-  int a, b;
+  int a{};
+  int b{};
 
   std::cin >> a >> b;
 
-  std::cout << a * (b % 100 % 10) << std::endl;
-  std::cout << a * (b % 100 / 10) << std::endl;
-  std::cout << a * (b / 100) << std::endl;
-  std::cout << a * b << std::endl;
+  int unitDigit = b % 10;
+  int tensDigit = (b / 10) % 10;
+  int hundredsDigit = b / 100;
+
+  int productByUnitDigit = a * unitDigit;
+  int productByTensDigit = a * tensDigit;
+  int productByHundredsDigit = a * hundredsDigit;
+  int finalProduct = a * b;
+
+  std::cout << productByUnitDigit << std::endl;
+  std::cout << productByTensDigit << std::endl;
+  std::cout << productByHundredsDigit << std::endl;
+  std::cout << finalProduct << std::endl;
 
   return 0;
 }
