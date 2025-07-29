78cf45a0c2f703aeba471f3a4a1e6f18f562513a
Kaeru224 <tmdn01cs@gmail.com>
2025-07-29 13:50:52 +0900

    [2438] 별 찍기 - 1

--- Baekjoon/2438/2438.cpp
+++ Baekjoon/2438/2438.cpp
@@ -8,8 +8,8 @@ int main() {
 
   std::cin >> n;
 
-  for (int i{1}; i <= n; ++i) {
-    for (int j{1}; j <= i; ++j) {
+  for (int line{1}; line <= n; ++line) {
+    for (int starCount{1}; starCount <= line; ++starCount) {
       std::cout << '*';
     }
     std::cout << '\n';
