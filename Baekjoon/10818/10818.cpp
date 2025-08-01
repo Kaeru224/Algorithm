8963f0ef5ccddc66a2ab9ae40da3581e20a2355f
Kaeru224 <tmdn01cs@gmail.com>
2025-08-02 02:39:48 +0900

    [10818] 최소, 최대

---
+++ Baekjoon/10818/10818.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n{};
+
+  std::cin >> n;
+
+  int firstNum{};
+
+  std::cin >> firstNum;
+
+  int minVal = firstNum;
+  int maxVal = firstNum;
+
+  for (int i{1}; i < n; ++i) {
+    int currentNum{};
+
+    std::cin >> currentNum;
+
+    minVal = std::min(minVal, currentNum);
+    maxVal = std::max(maxVal, currentNum);
+  }
+
+  std::cout << minVal << " " << maxVal << '\n';
+
+  return 0;
+}
