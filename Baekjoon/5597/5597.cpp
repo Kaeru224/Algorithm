a04106686db0e3ef8066b0ce69c4a027ad9fe2fa
Kaeru224 <tmdn01cs@gmail.com>
2025-08-05 22:37:20 +0900

    [5597] 과제 안 내신 분..?

---
+++ Baekjoon/5597/5597.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::vector<bool> students(31);
+
+  for (int i{1}; i <= 28; ++i) {
+    int studentNum{};
+    std::cin >> studentNum;
+    students[studentNum] = true;
+  }
+
+  for (int i{1}; i <= 30; ++i) {
+    if (students[i] == false) {
+      std::cout << i << '\n';
+    }
+  }
+
+  return 0;
+}
