641f441cbc13a8c87691bd0311a4176154610e82
Kaeru224 <tmdn01cs@gmail.com>
2025-08-09 20:47:21 +0900

    [10172] 개

--- Baekjoon/10172/10172.cpp
+++ Baekjoon/10172/10172.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-  std::cout << "|\\_/|" << std::endl;
-  std::cout << "|q p|   /}" << std::endl;
-  std::cout << "( 0 )\"\"\"\\" << std::endl;
-  std::cout << "|\"^\"`    |" << std::endl;
-  std::cout << "||_/=\\\\__|" << std::endl;
-
+  std::cout << R"&(|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|)&"
+            << '\n';
   return 0;
 }
