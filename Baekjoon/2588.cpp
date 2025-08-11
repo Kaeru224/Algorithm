2ea12ad1d58d58a764131efed0be4bd47d4ba021
Kaeru224 <tmdn01cs@gmail.com>
2025-08-12 00:23:56 +0900

    [2588] 곱셈

---
+++ Baekjoon/2588.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int a{};
+  int b{};
+
+  std::cin >> a >> b;
+
+  std::cout << b % 10 * a << '\n';
+  std::cout << b / 10 % 10 * a << '\n';
+  std::cout << b / 100 * a << '\n';
+  std::cout << a * b << '\n';
+
+  return 0;
+}
