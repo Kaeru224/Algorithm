ae0cd49caf82b102c7a275e6b1c2273ea27c15a8
Kaeru224 <tmdn01cs@gmail.com>
2025-08-13 11:48:10 +0900

    [10171] 고양이

---
+++ Baekjoon/10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::cout << R"(\    /\
+ )  ( ')
+(  /  )
+ \(__)|)" << '\n';
+
+  return 0;
+}
