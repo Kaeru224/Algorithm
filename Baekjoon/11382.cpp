364f2904f095da1532f852150a249bf1c7654462
Kaeru224 <tmdn01cs@gmail.com>
2025-08-13 11:41:23 +0900

    [11382] 꼬마 정민

---
+++ Baekjoon/11382.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  long long a{};
+  long long b{};
+  long long c{};
+
+  std::cin >> a >> b >> c;
+
+  long long sum = a + b + c;
+
+  std::cout << sum << '\n';
+
+  return 0;
+}
