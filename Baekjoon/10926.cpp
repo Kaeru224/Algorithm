886cbc7d8ea011ec9eeab280a836b880a9c9c1e9
Kaeru224 <tmdn01cs@gmail.com>
2025-08-14 00:10:34 +0900

    [10926] ??!

---
+++ Baekjoon/10926.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::string id;
+
+  std::cin >> id;
+
+  std::cout << id << "??!" << '\n';
+
+  return 0;
+}
