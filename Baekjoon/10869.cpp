863612fa5828d70045aaee6c5dbf6f49346f175f
Kaeru224 <tmdn01cs@gmail.com>
2025-08-13 11:48:55 +0900

    [10869] 사칙연산

---
+++ Baekjoon/10869.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int a{};
+  int b{};
+
+  std::cin >> a >> b;
+
+  std::cout << a + b << '\n';
+  std::cout << a - b << '\n';
+  std::cout << a * b << '\n';
+  std::cout << a / b << '\n';
+  std::cout << a % b << '\n';
+
+  return 0;
+}
