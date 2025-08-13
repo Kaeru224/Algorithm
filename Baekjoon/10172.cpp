b82560ea721e257a4f575720de8b57e7d59d7a2e
Kaeru224 <tmdn01cs@gmail.com>
2025-08-14 00:09:47 +0900

    [10172] 개

---
+++ Baekjoon/10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::cout << R"-(|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|)-"
+            << '\n';
+  return 0;
+}
